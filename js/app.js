0631c248d77fec1f7dd9c0bb83d3ceb22eca5b1e
fooku18 <jakob.schlonsok@yahoo.de>
2016-09-15 21:32:09 +0200

    newest update
    super

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 var app = angular.module("myApp",["myServices"]);
+app.run(["$pouchDB",function($pouchDB) {
+	$pouchDB.setDatabase("localDB");
+}]);
+app.controller("pouchyCtrl",["$scope","$rootScope","$pouchDB",function($scope,$rootScope,$pouchDB) {
+	$scope.items = {};
+	
+	$pouchDB.startListening();
+	
+	$scope.addItem = function() {
+		$pouchDB.addDefer($scope.doc).then(function(doc) {
+			alert(doc + " created!");
+		});
+	}
+	
+	//Global Listeners
+	//$rootScope.$on("$pouchDB:change", function(event,data) {
+	//	console.log(data);
+	//});
+}]);
--- js/service.js
+++ js/service.js
@@ -1,5 +1,5 @@
-var services = angular.module("myServices",[]);
-services.service("$pouchDB",["$rootScope","$q",function($rootScope,$q) {
+var myServices = angular.module("myServices",[]);
+myServices.service("$pouchDB",["$rootScope","$q",function($rootScope,$q) {
 	var database;
 	var changeListener;
 	
@@ -7,17 +7,40 @@ services.service("$pouchDB",["$rootScope","$q",function($rootScope,$q) {
 		database = new PouchDB(databaseName);
 	}
 	
+	this.addDefer = function(doc) {
+		var defer = $q.defer();
+		database.put({
+			"_id": new Date().toISOString(),
+			"name": doc
+		}).then(function() {
+			defer.resolve(doc);
+		},function() {
+			defer.reject(doc);
+		});
+		return defer.promise;
+	}
+	
 	this.startListening = function() {
 		changeListener = database.changes({
 			live: true,
 			include_docs: true
 		}).on("change"), function(change) {
-			if(!change.deleted) {
+			/*if(!change.deleted) {
 				$rootScope.$broadcast("$pouchDB:change",change);
 			} else {
 				$rootScope.$broadcast("$pouchDB:delete",change);
-			}
+			}*/
+			console.log(change);
 		}
 	}
+	
+	//this.stopListening = function() {
+	//	changeListener.cancel();
+	//}
+	
+	//this.save = function(doc) {
+	//	var deferred = $q.defer();
+	//	
+	//}
 	
 }]);
