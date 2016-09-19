edd35f8f9fac58c363531dedccb964d1fc1aec55
fooku18 <jakob.schlonsok@yahoo.de>
2016-09-19 09:19:01 +0200

    update 19092016

---
+++ js/directive.js
@@ -0,0 +1,10 @@
+app.directive("navi",["routeNavi",function(routeNavi) {
+	return {
+		restrict: "E",
+		replace: true,
+		templateUrl: "templates/navi_template.html",
+		controller: function($scope) {
+			$scope.routes = routeNavi.routes;
+		}
+	}
+}]);
